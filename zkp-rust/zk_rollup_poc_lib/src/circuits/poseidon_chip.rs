31cdc4f2da0be503facf0f6ae9d08e56be0b2467
HyoungsungKim <rktkek456@gmail.com>
2025-04-07 14:28:08 +0900

    Poseidon chip is implemented

--- zkp-rust/zk_rollup_poc_lib/src/circuits/poseidon_chip.rs
+++ zkp-rust/zk_rollup_poc_lib/src/circuits/poseidon_chip.rs
@@ -1,91 +1,93 @@
 use halo2_proofs::{
-    circuit::{AssignedCell, Layouter},
-    plonk::{Advice, Column, ConstraintSystem, Instance, Error, Fixed},
+    circuit::{AssignedCell, Layouter, Region, Value},
+    plonk::{Advice, Column, ConstraintSystem, Instance, Error},
     pasta::Fp,
 };
-use halo2_gadgets::poseidon::{Pow5Chip, Pow5Config};
-use halo2_gadgets::poseidon::primitives::P128Pow5T3;
+use halo2_gadgets::poseidon::{Hash, Pow5Chip, Pow5Config};
+use halo2_gadgets::poseidon::primitives::{Spec, ConstantLength};
 use std::convert::TryInto;
 
-use crate::constants::{POSEIDON_WIDTH, POSEIDON_RATE, POSEIDON_INPUTS};
 
-
-#[derive(Clone, Debug)]
-pub struct PoseidonChipConfig {
-    pub poseidon_config: Pow5Config<Fp, POSEIDON_WIDTH, POSEIDON_RATE>,
-    pub inputs: [Column<Advice>; POSEIDON_INPUTS],
-    /*
-    === Advice ===
-    TransactionConfig = {sender_balance_before, receiver_balance_before, transaction_amount, sender_balance_after, receiver_balance_after}
-    sender_address,
-    receiver_address,    
-     */
+#[derive(Debug, Clone)]
+pub struct PoseidonConfig<const WIDTH: usize, const RATE: usize, const L: usize> {
+    inputs: Vec<Column<Advice>>,
+    pow5_config: Pow5Config<Fp, WIDTH, RATE>,
 }
 
-pub struct PoseidonChip {
-    pub config: PoseidonChipConfig,
-    pub _marker: std::marker::PhantomData<Fp>,
+#[derive(Debug, Clone)]
+pub struct PoseidonChip<
+    S: Spec<Fp, WIDTH, RATE>,
+    const WIDTH: usize,
+    const RATE: usize,
+    const L: usize,
+> {
+    config: PoseidonConfig<WIDTH, RATE, L>,
+    _marker: std::marker::PhantomData<S>,
 }
 
-impl PoseidonChip {
-    pub fn construct(config: PoseidonChipConfig) -> Self {
+impl<S: Spec<Fp, WIDTH, RATE>, const WIDTH: usize, const RATE: usize, const L: usize> PoseidonChip<S, WIDTH, RATE, L>{
+    pub fn construct(config: PoseidonConfig<WIDTH, RATE, L>) -> Self {
         Self {
             config,
             _marker: std::marker::PhantomData,
         }
     }
 
-    pub fn configure_poseidon(meta: &mut ConstraintSystem<Fp>) -> Pow5Config<Fp, POSEIDON_WIDTH, POSEIDON_RATE> {
-        let state: [Column<Advice>; POSEIDON_WIDTH] =
-        (0..POSEIDON_WIDTH).map(|_| meta.advice_column()).collect::<Vec<_>>().try_into().unwrap();
-
+    pub fn configure(meta: &mut ConstraintSystem<Fp>) -> PoseidonConfig<WIDTH, RATE, L> {
+        let state = (0..WIDTH).map(|_| meta.advice_column()).collect::<Vec<_>>();
         let partial_sbox = meta.advice_column();
-
-        let rc_a: [Column<Fixed>; POSEIDON_WIDTH] =
-            (0..POSEIDON_WIDTH).map(|_| meta.fixed_column()).collect::<Vec<_>>().try_into().unwrap();
-
-        let rc_b: [Column<Fixed>; POSEIDON_WIDTH] =
-            (0..POSEIDON_WIDTH).map(|_| meta.fixed_column()).collect::<Vec<_>>().try_into().unwrap();
-
-    
-        Pow5Chip::<Fp, POSEIDON_WIDTH, POSEIDON_RATE>::configure::<P128Pow5T3>(
-            meta,
-            state,
-            partial_sbox,
-            rc_a,
-            rc_b,
-        )
-    }
-
-    pub fn configure(
-        meta: &mut ConstraintSystem<Fp>,
-        inputs: [Column<Advice>; POSEIDON_INPUTS],
-    ) -> PoseidonChipConfig {
-        let poseidon_config = Self::configure_poseidon(meta);
-
-        for col in inputs.clone().iter() {
-            meta.enable_equality(*col);
+        let rc_a = (0..WIDTH).map(|_| meta.fixed_column()).collect::<Vec<_>>();
+        let rc_b = (0..WIDTH).map(|_| meta.fixed_column()).collect::<Vec<_>>();
+        for i in 0..WIDTH {
+            meta.enable_equality(state[i]);
         }
+        meta.enable_constant(rc_b[0]);
 
-        PoseidonChipConfig {
-            poseidon_config,
-            inputs,
+        let pow5_config = Pow5Chip::configure::<S>(
+            meta,
+            state.clone().try_into().unwrap(),
+            partial_sbox.try_into().unwrap(),
+            rc_a.try_into().unwrap(),
+            rc_b.try_into().unwrap(),
+        );
+
+        PoseidonConfig {
+            inputs: state.clone().try_into().unwrap(),
+            pow5_config: pow5_config,
         }
     }
 
     pub fn hash(
         &self,
         mut layouter: impl Layouter<Fp>,
-        inputs: [AssignedCell<Fp, Fp>; POSEIDON_INPUTS],
+        words: &[AssignedCell<Fp, Fp>; L],
     ) -> Result<AssignedCell<Fp, Fp>, Error> {
-        let chip = Pow5Chip::<Fp, POSEIDON_WIDTH, POSEIDON_RATE>::construct(self.config.poseidon_config.clone());
+        let pow5_chip = Pow5Chip::construct(self.config.pow5_config.clone());
+        let word_cells = layouter.assign_region(
+            || "load words",
+            |mut region| -> Result<[AssignedCell<Fp, Fp>; L], Error> {
+                let result = words
+                    .iter()
+                    .enumerate()
+                    .map(|(i, word)| {
+                        word.copy_advice(
+                            || format!("word {}", i),
+                            &mut region,
+                            self.config.inputs[i],
+                            0,
+                        )
+                    })
+                    .collect::<Result<Vec<AssignedCell<Fp, Fp>>, Error>>();
+                Ok(result?.try_into().unwrap())
+            },
+        )?;
 
-        let hasher = halo2_gadgets::poseidon::Hash::<Fp, _, P128Pow5T3, _, POSEIDON_WIDTH, POSEIDON_RATE>
-            ::init(chip, layouter.namespace(|| "poseidon"))?;
-        
-        hasher.hash(layouter.namespace(|| "poseidon hash"), inputs)
+        let hasher = Hash::<_, _, S, ConstantLength<L>, WIDTH, RATE>::init(
+            pow5_chip,
+            layouter.namespace(|| "hasher"),
+        )?;
+        hasher.hash(layouter.namespace(|| "hash"), word_cells)
     }
-    
 
     pub fn expose_public(
         &self,
@@ -96,6 +98,23 @@ impl PoseidonChip {
     ) -> Result<(), Error> {
         layouter.constrain_instance(hash.cell(), instance, row)
     }
+
+    pub fn assign(
+        &self,
+        region: &mut Region<'_, Fp>,
+        offset: usize,
+        inputs: [Value<Fp>; L]
+    ) -> Result<(), Error> {
+        for (i, input) in inputs.iter().enumerate() {
+            region.assign_advice(
+                || format!("input {}", i),
+                self.config.inputs[i],
+                offset,
+                || *input
+            )?;
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -105,84 +124,74 @@ mod tests {
         circuit::{Layouter, SimpleFloorPlanner, Value},
         dev::MockProver,
         pasta::Fp,
-        plonk::{Circuit, ConstraintSystem, Error},
+        plonk::{Circuit, ConstraintSystem, Error, Instance},
     };
-    use halo2_gadgets::poseidon::primitives::{ConstantLength, Hash as PoseidonPrimitiveHash};
+    use halo2_gadgets::poseidon::primitives::{P128Pow5T3, ConstantLength, Hash as PoseidonPrimitiveHash};
+
+    const WIDTH: usize = 3;
+    const RATE: usize = 2;
+    const L: usize = 2;
 
     #[derive(Default)]
     struct DummyPoseidonCircuit {
-        pub inputs: [Value<Fp>; POSEIDON_INPUTS],
+        inputs: [Value<Fp>; L],
     }
 
     impl Circuit<Fp> for DummyPoseidonCircuit {
-        type Config = (PoseidonChipConfig, Column<Instance>);
+        type Config = (PoseidonConfig<WIDTH, RATE, L>, Column<Instance>);
         type FloorPlanner = SimpleFloorPlanner;
 
         fn without_witnesses(&self) -> Self {
             Self {
-                inputs: [Value::unknown(); POSEIDON_INPUTS],
+                inputs: [Value::unknown(); L],
             }
         }
 
         fn configure(meta: &mut ConstraintSystem<Fp>) -> Self::Config {
             let instance = meta.instance_column();
             meta.enable_equality(instance);
-
-            let inputs: [Column<Advice>; POSEIDON_INPUTS] = core::array::from_fn(|_| meta.advice_column());
-            let chip_config = PoseidonChip::configure(meta, inputs);
-
+            let chip_config = PoseidonChip::<P128Pow5T3, WIDTH, RATE, L>::configure(meta);
             (chip_config, instance)
         }
 
         fn synthesize(
             &self,
-            (chip_config, instance): Self::Config,
+            config: Self::Config,
             mut layouter: impl Layouter<Fp>,
         ) -> Result<(), Error> {
-            let chip = PoseidonChip::construct(chip_config.clone());
+            let (chip_config, instance) = config;
+            let chip = PoseidonChip::<P128Pow5T3, WIDTH, RATE, L>::construct(chip_config);
 
-            let assigned_inputs: [AssignedCell<Fp, Fp>; POSEIDON_INPUTS] =
-                layouter.assign_region(
-                    || "load inputs",
-                    |mut region| {
-                        self.inputs
-                            .iter()
-                            .enumerate()
-                            .map(|(i, val)| {
-                                region.assign_advice(
-                                    || format!("input {}", i),
-                                    chip_config.inputs[i],
-                                    0,
-                                    || *val,
-                                )
-                            })
-                            .collect::<Result<Vec<_>, _>>()
-                            .and_then(|cells| Ok(cells.try_into().unwrap()))
-                    },
-                )?;
+            let message_cells: [AssignedCell<Fp, Fp>; L] = layouter.assign_region(
+                || "load inputs",
+                |mut region| {
+                    self.inputs
+                        .iter()
+                        .enumerate()
+                        .map(|(i, val)| {
+                            region.assign_advice(
+                                || format!("input {}", i),
+                                chip.config.inputs[i],
+                                0,
+                                || *val,
+                            )
+                        })
+                        .collect::<Result<Vec<_>, _>>()
+                        .and_then(|cells| cells.try_into().map_err(|_| Error::Synthesis))
+                },
+            )?;
 
-            let hash_result = chip.hash(layouter.namespace(|| "poseidon hash"), assigned_inputs)?;
+            let hash_result = chip.hash(layouter.namespace(|| "poseidon hash"), &message_cells)?;
             chip.expose_public(&mut layouter, &hash_result, instance, 0)?;
-
             Ok(())
         }
     }
 
     #[test]
     fn test_poseidon_hash_works() {
-        let inputs_fp: [Fp; POSEIDON_INPUTS] = [
-            Fp::from(100), // sender_balance_before
-            Fp::from(0),   // receiver_balance_before
-            Fp::from(50),  // transaction_amount
-            Fp::from(50),  // sender_balance_after
-            Fp::from(50),  // receiver_balance_after
-            Fp::from(123), // sender_address
-            Fp::from(456), // receiver_address
-        ];
-
-        // 예상되는 해시를 primitive로 계산
+        let inputs_fp: [Fp; L] = [Fp::from(100), Fp::from(0)];
         let expected_hash = {
-            let hasher = PoseidonPrimitiveHash::<_, P128Pow5T3, ConstantLength<POSEIDON_INPUTS>, POSEIDON_WIDTH, POSEIDON_RATE>::init();
+            let hasher = PoseidonPrimitiveHash::<_, P128Pow5T3, ConstantLength<L>, WIDTH, RATE>::init();
             hasher.hash(inputs_fp)
         };
 
@@ -191,7 +200,13 @@ mod tests {
         };
 
         let public_inputs = vec![vec![expected_hash]];
-        let prover = MockProver::run(9, &circuit, public_inputs).unwrap();
-        prover.assert_satisfied();
+        let prover = MockProver::run(10, &circuit, public_inputs);
+        match prover {
+            Ok(prover) => {
+                prover.assert_satisfied();
+                println!("Test passed successfully!");
+            }
+            Err(e) => panic!("MockProver failed with error: {:?}", e),
+        }
     }
 }
--- zkp-rust/zk_rollup_poc_lib/src/constants.rs
+++ zkp-rust/zk_rollup_poc_lib/src/constants.rs
@@ -2,4 +2,4 @@ pub const MAX_BALANCE: usize = 1000;
 
 pub const POSEIDON_WIDTH: usize = 3;
 pub const POSEIDON_RATE: usize = 2;
-pub const POSEIDON_INPUTS: usize = 7;
+pub const POSEIDON_INPUTS: usize = 2; //POSEIDON_RATE;
